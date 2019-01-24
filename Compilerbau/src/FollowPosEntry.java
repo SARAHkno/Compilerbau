b568343b58baf229b259388babdbbedcb4b7900b
Sari <43409873+SARAHkno@users.noreply.github.com>
2019-01-24 14:58:54 +0100

    Gegebener Code Eingefügt
    Fehlerausgabe für Visitor, gegebener Code muss gaändert werden um Fehler zu beheben?

---
+++ Compilerbau/src/FollowPosEntry.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class FollowposTableEntry
+{
+    public final int position;
+    public final String symbol;
+    public final Set<Integer> followpos = new HashSet<>();
+    public FollowposTableEntry(int position, String symbol)
+    {
+        this.position = position;
+        this.symbol = symbol;
+    }
+}
