10468903cda7cd4b09e5b28280affdc9e9744d31
Sari <43409873+SARAHkno@users.noreply.github.com>
2019-02-04 13:11:40 +0100

    Hey Leute ich habe probleme erzeugt. Ich weiß nicht was unter IVisible und was unter Syntax node implementiertwerden muss, methoden sind teilweise doppelt...

--- src/com/dhbw/thinf/compilerbau/node/UnaryOpNode.java
+++ src/com/dhbw/thinf/compilerbau/node/UnaryOpNode.java
@@ -1,11 +1,18 @@
 package com.dhbw.thinf.compilerbau.node;
 
-public class UnaryOpNode {
+import com.dhbw.thinf.compilerbau.visitor.IVisitor;
+
+public class UnaryOpNode extends SyntaxNode{
     private String operator;
     private IVisitable subNode;
 
     public UnaryOpNode(String operator, IVisitable subNode) {
         this.operator = operator;
         this.subNode = subNode;
+    }
+
+    public void accept(IVisitor visitor)
+    {
+        visitor.visit(this);
     }
 }
