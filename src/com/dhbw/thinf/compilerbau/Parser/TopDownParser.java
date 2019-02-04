8232523f21efac07a9eaeed9b07d0fce5840bb26
Rico Kraft <rico3004@gmx.de>
2019-02-04 13:22:58 +0100

    Implemented read method for the regular Expression in Parser Class

--- src/com/dhbw/thinf/compilerbau/Parser/TopDownParser.java
+++ src/com/dhbw/thinf/compilerbau/Parser/TopDownParser.java
@@ -1,10 +1,25 @@
 package com.dhbw.thinf.compilerbau.Parser;
 
 import com.dhbw.thinf.compilerbau.node.IVisitable;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TopDownParser {
     private IVisitable tree;
 
     public TopDownParser() {
+    }
+
+    private String readRegEx() throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("res" + System.getProperty("file.separator" + "regEx.txt")))) {
+            return bufferedReader.readLine();
+        }
+    }
+
+    public void parse() throws IOException {
+        String inputLine = readRegEx();
+
     }
 }
