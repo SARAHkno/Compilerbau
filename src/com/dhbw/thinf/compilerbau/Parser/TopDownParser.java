1de369b9a5f4b9eebff407a1281d6cba1de16b08
Rico Kraft <rico3004@gmx.de>
2019-02-04 13:06:09 +0100

    Imports adjusted

--- src/com/dhbw/thinf/compilerbau/Parser/TopDownParser.java
+++ src/com/dhbw/thinf/compilerbau/Parser/TopDownParser.java
@@ -1,14 +1,10 @@
 package com.dhbw.thinf.compilerbau.Parser;
 
 import com.dhbw.thinf.compilerbau.node.IVisitable;
-import com.dhbw.thinf.compilerbau.node.SyntaxNode;
-import java.util.TreeMap;
 
 public class TopDownParser {
-    private TreeMap<Integer, SyntaxNode> syntaxtTree;
-    private IVisitable root;
+    private IVisitable tree;
 
     public TopDownParser() {
-        syntaxtTree = new TreeMap<>();
     }
 }
