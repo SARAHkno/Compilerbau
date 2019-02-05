a455a0891a7d892496c0520599b520cd284042d8
estellegraef <estellegraef@users.noreply.github.com>
2019-02-05 09:40:04 +0100

    Second visitor created

---
+++ src/com/dhbw/thinf/compilerbau/visitor/TableVisitor.java
@@ -0,0 +1,11 @@
+package com.dhbw.thinf.compilerbau.visitor;
+
+import com.dhbw.thinf.compilerbau.node.SyntaxNode;
+
+public class TableVisitor implements IVisitor{
+
+    @Override
+    public void visit(SyntaxNode node) {
+
+    }
+}
