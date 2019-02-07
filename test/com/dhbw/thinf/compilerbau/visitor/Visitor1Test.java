430abd6ed37e396cde5db6e82bc73ae921098c12
Julius Schinschke <juliuss.16303@gmail.com>
2019-02-07 11:23:10 +0100

    Create Visitor test

---
+++ test/com/dhbw/thinf/compilerbau/visitor/Visitor1Test.java
@@ -0,0 +1,30 @@
+package com.dhbw.thinf.compilerbau.visitor;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Visitor1Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void visit() {
+    }
+
+    @Test
+    void visit1() {
+    }
+
+    @Test
+    void visit2() {
+    }
+}
