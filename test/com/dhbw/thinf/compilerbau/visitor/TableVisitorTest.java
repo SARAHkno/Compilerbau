4db40cdfad91b04f14c62d821e36c38b03eb3a9f
Julius Schinschke <juliuss.16303@gmail.com>
2019-02-11 17:21:24 +0100

    TableVisitor BinOpNode Test - not finished yet

--- test/com/dhbw/thinf/compilerbau/visitor/TableVisitorTest.java
+++ test/com/dhbw/thinf/compilerbau/visitor/TableVisitorTest.java
@@ -1,8 +1,9 @@
 package com.dhbw.thinf.compilerbau.visitor;
 
+import com.dhbw.thinf.compilerbau.node.BinOpNode;
 import com.dhbw.thinf.compilerbau.node.IVisitable;
 import com.dhbw.thinf.compilerbau.node.OperandNode;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.SortedMap;
 
@@ -14,14 +15,29 @@ class TableVisitorTest {
     void getOpNodeTable() {
         String symbol = "z";
         IVisitable operandNode = new OperandNode(symbol);
-        TableVisitor tableVisitor = new TableVisitor();
+        IVisitor tableVisitor = new TableVisitor();
         tableVisitor.visit(operandNode);
-        SortedMap<Integer, FollowposTableEntry> getGeneratedMap =  tableVisitor.getTable();
+        SortedMap<Integer, FollowposTableEntry> getGeneratedMap = ((TableVisitor) tableVisitor).getTable();
         assertEquals(1, getGeneratedMap.size());
         assertEquals(symbol, getGeneratedMap.get(1).symbol);
     }
 
     @Test
-    void getTable() {
+    void getBinOpNodeTable() {
+        IVisitable node1 = new OperandNode("a");
+        IVisitable node2 = new OperandNode(")");
+        IVisitor visitor = new TableVisitor();
+        visitor.visit(node1);
+        visitor.visit(node2);
+
+        IVisitable binOpNode = new BinOpNode("+", node1, node2);
+        visitor.visit(binOpNode);
+        SortedMap<Integer, FollowposTableEntry> generatedMap = ((TableVisitor) visitor).getTable();
+        assertEquals(2, generatedMap.size());
+
+        binOpNode = new BinOpNode("°", node1, node2);
+        visitor.visit(binOpNode);
+        generatedMap = ((TableVisitor) visitor).getTable();
+        assertEquals(2, generatedMap.size());
     }
 }
