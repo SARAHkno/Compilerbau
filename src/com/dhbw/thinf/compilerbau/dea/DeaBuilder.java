084c7cbb56cd557c0106157c052bcc8d3ac73817
Rico Kraft <rico3004@gmx.de>
2019-02-11 10:38:49 +0100

    TransitionTable added

--- src/com/dhbw/thinf/compilerbau/dea/DeaBuilder.java
+++ src/com/dhbw/thinf/compilerbau/dea/DeaBuilder.java
@@ -1,4 +1,13 @@
 package com.dhbw.thinf.compilerbau.dea;
 
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class DeaBuilder implements IDeaBuilder {
+    private SortedMap<DFAState, Map<Character, DFAState>> stateTransitionTable;
+
+    public DeaBuilder(){
+        stateTransitionTable = new TreeMap<>();
+    }
 }
